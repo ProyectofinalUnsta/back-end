f4c7b34fcbbb73830d008a69609ee2f542844365
Mateo <mateobinance1@gmail.com>
2025-04-14 15:55:23 -0300

    correccion de algunos errores

--- src/api/server.js
+++ src/api/server.js
@@ -1,14 +1,16 @@
 import express from 'express'
-import router from './router/router.js'
-import data from './const/const.js'
+import router from '../router/router.js'
+import data from '../const/const.js'
 import cookieParser from 'cookie-parser'
-import { mongooseconection } from './conection/mongoconection.js'
+import { mongooseconection } from '../conection/mongoconection.js'
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cookieParser())
 mongooseconection()
-app.use('/', router)
-
+app.use('/api', router)
+app.get('/', (req, res) => {
+  res.send('server is working!')
+})
 app.listen((data.port), () => { console.log('server is running') })
