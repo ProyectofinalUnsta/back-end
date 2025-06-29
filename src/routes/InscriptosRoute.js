a5615bae368958d9be04afb16a2f00215f42deda
Mateo <mateobinance1@gmail.com>
2025-06-28 21:13:29 -0300

    cambio de orden get

--- src/routes/InscriptosRoute.js
+++ src/routes/InscriptosRoute.js
@@ -4,8 +4,9 @@ import { InscriptosController } from '../controller/InscriptosController.js'
 const InscriptosRoute = express.Router()
 
 InscriptosRoute.post('/', InscriptosController.InscribirmeEvento)
-InscriptosRoute.get('/:gmail',InscriptosController.getArchivosInscripto)
 InscriptosRoute.get('/inscripto',InscriptosController.getInscripcionPorEvento)
+InscriptosRoute.get('/:gmail',InscriptosController.getArchivosInscripto)
+
 
 
 export default InscriptosRoute
