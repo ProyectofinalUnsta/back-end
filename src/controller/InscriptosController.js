47ec3c18e504f99b9684e35ecd4fb29882f32082
Mateo <mateobinance1@gmail.com>
2025-07-02 20:49:06 -0300

    ffxing this shit

--- src/controller/InscriptosController.js
+++ src/controller/InscriptosController.js
@@ -31,7 +31,7 @@ export class InscriptosController {
         try {
           const IdEventosInscriptos = await Inscriptos.find({gmail:req.params.gmail})
           console.log(IdEventosInscriptos)
-       const MappedIdEventosInscripto = IdEventosInscriptos.map(inscripto => mongoose.Types.ObjectId(inscripto.idEvento))
+       const MappedIdEventosInscripto = IdEventosInscriptos.map(inscripto => new mongoose.Types.ObjectId(inscripto.idEvento))
            console.log( 'set de IDS', IdEventosInscriptos)
         const Archivos = await Presentation.find({event:{ $in: MappedIdEventosInscripto}})
    
